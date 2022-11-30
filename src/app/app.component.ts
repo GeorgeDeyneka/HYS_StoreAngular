bf03c095c75662aaa20239b5190d4bba9692a8fa
GeorgeDeyneka <zhoradeynecka@gmail.com>
2022-12-01 00:35:10 +0200

    v. 1.0.0 | Create the main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { state } from './data/state';
+import { Product } from './models/interfaces/products.interface';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'HYS_AngularStore';
+  protected data: Product[] = state || createRandomData(8);
+}
+
+function createRandomData(n: number) {
+  const arr: Product[] = [];
+
+  for (let i = 1; i <= n; i++) {
+    const randomStr: string = (Math.random() + 1).toString(36).substring(2);
+    const randomNum: number = Math.floor(Math.random() * (2000 - 1000)) + 1000;
+
+    const obj: Product = {
+      id: i,
+      name: randomStr,
+      price: randomNum
+    };
+
+    arr.push(obj);
+  }
+  return arr;
 }
