b0f3ecc9930089ab0a38e9faa4d91d6cd97ee806
GeorgeDeyneka <zhoradeynecka@gmail.com>
2022-12-01 17:41:15 +0200

    ls_1 | Refactoring createRandomData function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,23 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  protected data: Product[] = state || createRandomData(8);
-}
+  protected data: Product[] = state || this.createRandomData(8);
 
-function createRandomData(n: number) {
-  const arr: Product[] = [];
-
-  for (let i = 1; i <= n; i++) {
-    const randomStr: string = (Math.random() + 1).toString(36).substring(2);
-    const randomNum: number = Math.floor(Math.random() * (2000 - 1000)) + 1000;
-
-    const obj: Product = {
-      id: i,
-      name: randomStr,
-      price: randomNum
-    };
-
-    arr.push(obj);
+  createRandomData(n: number) {
+    const arr: Product[] = [];
+  
+    for (let i = 1; i <= n; i++) {
+      const randomStr: string = (Math.random() + 1).toString(36).substring(2);
+      const randomNum: number = Math.floor(Math.random() * (2000 - 1000)) + 1000;
+  
+      const obj: Product = {
+        id: i,
+        name: randomStr,
+        price: randomNum
+      };
+  
+      arr.push(obj);
+    }
+    return arr;
   }
-  return arr;
 }
+
