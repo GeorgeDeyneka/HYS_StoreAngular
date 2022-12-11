e46afc9c82aa6ecbeb7cc698780485e9b72b0df0
GeorgeDeyneka <zhoradeynecka@gmail.com>
2022-12-11 04:51:35 +0200

    ls_3 | Minor style changes

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,3 +1,4 @@
+import { CartService } from './../../shared/services/cart.service';
 import { DetailsService } from './../../shared/services/details.service';
 import { Product } from './../../models/interfaces/products.interface';
 import { Component, OnInit, Input } from '@angular/core';
@@ -5,15 +6,21 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
   public prod: Product;
 
-  constructor(private detailsService: DetailsService) { }
+  constructor(
+    private detailsService: DetailsService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
-    this.prod = this.detailsService.getData()
+    this.prod = this.detailsService.getData();
   }
 
+  addToCart() {
+    this.cartService.addToCart(this.prod);
+  }
 }
