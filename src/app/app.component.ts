a7b7539838849fb97e655a5d894aa58a008819a8
GeorgeDeyneka <zhoradeynecka@gmail.com>
2023-05-25 23:08:25 +0300

    Add scroll-up method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.scrollUp();
+  }
+
+  scrollUp() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+}
